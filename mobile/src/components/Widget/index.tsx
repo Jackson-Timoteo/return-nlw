4d70b9e342b95dfe5439f2bc9225b7f7f6e9c7a8
jackson-timoteo <jackson_siilva@hotmail.com>
2022-05-16 23:59:00 -0300

    Icons  app

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -2,10 +2,16 @@ import React, { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import { Options } from './Options';
+import { Form } from '../Form';
+
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes'
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
  function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -31,7 +37,13 @@ import { theme } from '../../theme';
       <BottomSheet
         ref={bottomSheetRef}
         snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
       >
+
+        <Form 
+          feedbackType='BUG'
+        />
 
       </BottomSheet>
     </>
