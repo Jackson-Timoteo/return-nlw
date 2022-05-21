894ab5c7cf5bffee05b1dcb4d9b3efca1623db94
jackson-timoteo <jackson_siilva@hotmail.com>
2022-05-21 13:22:13 -0300

    api

--- web/src/libs/api.ts
+++ web/src/libs/api.ts
@@ -1,5 +1,8 @@
  import axios from "axios";
 
  export const api = axios.create({
-     baseURL: import.meta.env.VITE_API_URL,
+     baseURL:'http://localhost:3333',
  })
+
+
+ //import.meta.env.VITE_API_URL
